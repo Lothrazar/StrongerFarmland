2138bfee11d9ebbe5eb7fcbc960b4234c98cf241
lothrazar <samson.bassett@gmail.com>
2021-07-25 07:13:17 -0700

    1.17 update and test

--- src/main/java/com/lothrazar/strongfarmland/StrongFarmland.java
+++ src/main/java/com/lothrazar/strongfarmland/StrongFarmland.java
@@ -1,16 +1,15 @@
 package com.lothrazar.strongfarmland;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.FarmlandBlock;
-import net.minecraft.entity.passive.IronGolemEntity;
-import net.minecraft.entity.passive.TameableEntity;
-import net.minecraft.entity.passive.horse.AbstractHorseEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(StrongFarmland.MODID)
@@ -27,36 +26,29 @@ public class StrongFarmland {
   public void onFarmlandTrampleEvent(BlockEvent.FarmlandTrampleEvent event) {
     // do something when the server start
     BlockState old = event.getWorld().getBlockState(event.getPos());
-    if (old.hasProperty(FarmlandBlock.MOISTURE) &&
-        old.get(FarmlandBlock.MOISTURE) > 0) {
+    if (old.hasProperty(FarmBlock.MOISTURE) &&
+        old.getValue(FarmBlock.MOISTURE) > 0) {
       // normally 0 dry, 7 wet
-      if (event.getEntity() instanceof PlayerEntity && !ConfigHandler.PLAYER.get()) {
+      if (event.getEntity() instanceof Player && !ConfigHandler.PLAYER.get()) {
         event.setCanceled(true);
       }
-      if (event.getEntity() instanceof TameableEntity) {
-        TameableEntity tamed = (TameableEntity) event.getEntity();
-        if (tamed.isTamed() && !ConfigHandler.TAMEABLE.get()) {
+      if (event.getEntity() instanceof TamableAnimal) {
+        TamableAnimal tamed = (TamableAnimal) event.getEntity();
+        if (tamed.isTame() && !ConfigHandler.TAMEABLE.get()) {
           event.setCanceled(true);
         }
       }
-      if (event.getEntity() instanceof AbstractHorseEntity) {
-        AbstractHorseEntity tamed = (AbstractHorseEntity) event.getEntity();
-        if (tamed.isTame() && !ConfigHandler.HORSE.get()) {
+      if (event.getEntity() instanceof AbstractHorse) {
+        AbstractHorse tamed = (AbstractHorse) event.getEntity();
+        if (tamed.isTamed() && !ConfigHandler.HORSE.get()) {
           event.setCanceled(true);
         }
       }
-      if (event.getEntity() instanceof IronGolemEntity
+      if (event.getEntity() instanceof IronGolem
           && !ConfigHandler.GOLEM.get()) {
         event.setCanceled(true);
       }
     }
   }
 
-  @SubscribeEvent
-  public static void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-    // https://tutorials.darkhax.net/tutorials/jar_signing/
-    String source = (event.getSource() == null) ? "" : event.getSource().getName() + " ";
-    String msg = MODID + "Invalid fingerprint detected! The file " + source + "may have been tampered with. This version will NOT be supported by the author!";
-    System.out.println(msg);
-  }
 }
